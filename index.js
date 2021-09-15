16e6d3a16942ec157c0eec9af384f3ea432e6033
kayy0812 <levanloc8112@gmail.com>
2021-09-15 23:25:50 +0700

    updating...

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ const config = require('./config.json');
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const queues = new Map();
-const simsimi = require('simsimi')({
-    key: config.simsimi_license_key
-});
-
 var bot = new Discord.Client();
-const responseMessage = {
-    "chào bot": "Bot Z xin chào bạn nha",
-    "bạn là trai hả": "Cậu chủ mình sao mình vậy",
-    "bạn ngu quá": "sao bạn lại chửi một con bot bé bỏng như mìn :(",
-    "ai là idol của bạn": "Là Dẽo CT á <3, mình thích chú Dẽo chơi game kinh dị cực :3"
-};
 
 bot.on('ready', function() {
-    console.log('BOT dang hoat dong ! ' + bot.user.tag);
+    console.log('Dang su dung BOT[' + bot.user.tag + ']');
 });
-
 
 class Queue {
     constructor(voiceChannel) {
@@ -38,43 +27,30 @@ class Song {
 }
 
 bot.on("message", async message => {
-    if (message.content.startsWith(config.bot_prefix)) {
-        const message_respone = message.content.slice(config.bot_prefix.length).trim();
-        if (responseMessage[message_respone]) {
-            message.reply(responseMessage[message_respone]);
-        }
-    }
 
-    if (message.content.startsWith(config.sim_prefix)) {
-        const message_to_sim = message.content.slice(config.sim_prefix.length).trim();
-        console.log('Send to Simsimi: ', message_to_sim);
-        (async () => {
-            const sim_response = await simsimi(message_to_sim);
-            message.reply('[SIMSIMI] - ' + sim_response); // What's up ?
-
-        })();
-        //message.reply('Chức năng nói chuyện Simsimi đang khoá để tiến hành hoàn thiện');
-    }
-
+    // Youtube MP3
     if (message.content.startsWith(config.music_prefix)) {
         const args = message.content.slice(config.music_prefix.length).trim().split(/ +/);
         const command = args[0].toLowerCase();
+        let serverQueue = queues.get(message.guild.id);
         if (command === 'play') {
             let voiceChannel = message.member.voice.channel;
             if (!voiceChannel || voiceChannel.name !== config.music_channel_name) {
-                return message.reply('Vào kênh `🎶' + config.music_channel_name + '🎶` để có thể nghe nhạc!');
+                message.reply('Vào kênh `🎶' + config.music_channel_name + '🎶` để có thể nghe nhạc!');
+                return false;
             }
             let permissions = voiceChannel.permissionsFor(message.client.user);
             if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-                return message.reply('Chưa cấp quyền vào kênh phát này!');
+                message.reply('Chưa cấp quyền vào kênh phát này!');
+                return false;
             }
             let url = args.slice(1).join(' ');
             let video = await ytdl.getInfo(url);
             if (!video) {
-                return message.reply('Url không hợp lệ!');
+                message.reply('Url không hợp lệ!');
+                return false;
             }
             const song = new Song(video.videoDetails.title, video.videoDetails.video_url);
-            const serverQueue = queues.get(message.guild.id);
             if (!serverQueue) {
                 let queue = new Queue(voiceChannel);
                 queues.set(message.guild.id, queue);
@@ -82,67 +58,70 @@ bot.on("message", async message => {
                 let connection = await voiceChannel.join();
                 queue.connection = connection;
                 playSong(message);
-            } else {
-                serverQueue.songs.push(song);
-                message.channel.send('🎶 Đã thêm vào danh sách chờ: `' + song.title + '`');
+                return true;
             }
-            return
+            serverQueue.songs.push(song);
+            message.channel.send('🎶 Đã thêm vào danh sách chờ: `' + song.title + '`');
         }
+
         if (command === 'stop') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể dừng lại!');
+            if (!serverQueue) {
+                message.reply('Không thể dừng lại!');
+                return false;
+            }
             serverQueue.songs = [];
             serverQueue.connection.dispatcher.end();
-            return
         }
+
         if (command === 'next') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể next lúc này!');
+            if (!serverQueue) {
+                message.reply('Không thể next lúc này!');
+                return false;
+            }
             serverQueue.connection.dispatcher.end();
             message.channel.send('🎶 Đang dừng: `' + serverQueue.songs[0].title + '`');
-            return
         }
+
         if (command === 'pause') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể tạm dừng!');
+            if (!serverQueue) {
+                message.reply('Không thể tạm dừng!');
+                return false;
+            }
             serverQueue.connection.dispatcher.pause();
-            return
         }
+
         if (command === 'resume') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể tiếp tục!');
+            if (!serverQueue) {
+                message.reply('Không thể tiếp tục!');
+                return false;
+            }
             serverQueue.connection.dispatcher.resume();
-            return
         }
-        if (command === 'repeat') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể lặp lại!');
+        
+        if (command === 'loop') {
+            if (!serverQueue) {
+                message.reply('Không thể lặp lại!');
+                return false;
+            }
+            if (serverQueue.repeat) {
+                serverQueue.repeat = false;
+                message.channel.send('🎶 Tắt lặp: `' + serverQueue.songs[0].title + '`');
+                return true;
+            }
             serverQueue.repeat = true;
-            message.channel.send('🎶 Lặp lại bài: `' + serverQueue.songs[0].title + '`');
-            return
+            message.channel.send('🎶 Bật lặp: `' + serverQueue.songs[0].title + '`');
         }
-        if (command === 'offrepeat') {
-            const serverQueue = queues.get(message.guild.id);
-            if (!serverQueue) return message.reply('Không thể tắt lặp lại!');
-            serverQueue.repeat = false;
-            message.channel.send('🎶 Ngừng lặp lại bài: `' + serverQueue.songs[0].title + '`');
-            return
-        }
+
         if (command === 'queue') {
-            const serverQueue = queues.get(message.guild.id)
-            if (!serverQueue) return message.reply('Hiện danh sách trống!');
+            if (!serverQueue) {
+                message.reply('Hiện danh sách trống!');
+                return false;
+            }
             let result = serverQueue.songs.map((song, i) => {
                 return `${(i == 0) ? `\`🎶 Đang phát:\`` : `${i}.`} ${song.title}`
             }).join('\n');
             message.channel.send(result);
-            return
         }
-    }
-
-
-    if (message.content === 'hello') {
-        message.react('❤️');
-        message.reply('Chào bạn mình rất vui khi được trò chuyện cùng với bạn <3');
     }
 });
 
@@ -152,20 +131,21 @@ async function playSong(message) {
     if (serverQueue.songs.length < 1) {
         serverQueue.voiceChannel.leave();
         queues.delete(message.guild.id);
-        return message.channel.send("Hết nhạc!");
+        message.channel.send("Hết nhạc!");
+        return true;
     }
     let song = serverQueue.songs[0];
     let dispatcher = serverQueue.connection.play(ytdl(song.url, {
         filter: 'audioonly',
-        dlChunkSize: 0
+        highWaterMark: 1024 * 1024 * 12
     }));
     dispatcher.setVolume(serverQueue.volume / 100);
     message.channel.send('🎶 Đang phát: `' + song.title + '`');
     dispatcher.on('finish', () => {
         if (!serverQueue.repeat) serverQueue.songs.shift();
-        return playSong(message);
+        playSong(message);
+        return true;
     });
 }
-
 
 bot.login(config.discord_token);
