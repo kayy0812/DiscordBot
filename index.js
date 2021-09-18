ab32386f4629432e2a3153969a8ff107d46bb5c2
kayy0812 <levanloc8112@gmail.com>
2021-09-18 11:52:59 +0700

    update

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const config = require('./config.json');
 const Discord = require('./Discord');
 
 // Youtube music
-const {loadMusic} = require('./ytMusic/ytMusic');
+const { loadMusic } = require('./ytMusic/ytMusic');
+const { loadSimsimi } = require('./simsimi/simsimi');
 Discord.setToken(process.env.TOKEN);
-
 Discord.onMessage(async message => {
     if (message.content.startsWith(config.musicPrefix)) {
         const args = message.content.slice(config.musicPrefix.length).trim().split(/ +/);
         loadMusic(config, args, message);
+    }
+    if (message.content.startsWith(config.simPrefix)) {
+        const args = message.content.slice(config.simPrefix.length).trim();
+        console.log(args)
+        loadSimsimi(config, args, message);
     }
 });
---
+++ simsimi/simsimi.js
@@ -0,0 +1,14 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const simsimi = {
+    loadSimsimi: async function (config, args, message) {
+        const response = await fetch(`https://api.simsimi.net/v2/?text=${args}&lc=en&cf=true`);
+        const data = await response.json();
+        
+        for (const msg of data.messages) {
+            message.reply(msg.text);
+        }
+    }
+}
+
+module.exports = simsimi;
