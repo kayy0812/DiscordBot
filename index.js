2ac7c68d2189240da646b98df5374fbb0028f33f
kayy0812 <kayy0812.levanloc@gmail.com>
2021-12-09 17:30:32 +0700

    updating simpleeeeeeeeeee.......

--- index.js
+++ index.js
@@ -1,69 +1,73 @@
-require('dotenv').config();
 // Discord Libraries
-const { prefix, commands } = require('./config.json');
-const { onMessage, setToken } = require('./Discord');
+const { prefix, token } = require('./config.json');
+const { Client, Intents} = require('discord.js');
+const { Player } = require("discord-music-player");
 
-// Heroku suporter
-const { wakeDyno } = require('heroku-keep-awake');
-
-// Express
-const express = require('express');
-const app = express();
-app.listen(process.env.PORT || 3000, () => {
-    wakeDyno(process.env.HEROKU_APP);
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ] 
 });
 
-// Login to Discord
-setToken(process.env.TOKEN);
+const player = new Player(client, {
+    leaveOnEmpty: false
+});
 
-// Features Commands
-const { 
-    playCommand,
-    listCommand,
-    pauseCommand,
-    resumeCommand,
-    nextCommand,
-    clearCommand,
-    loopCommand
-} = require('./ytMusic/ytMusic');
-const { 
-    simCommand 
-} = require('./simsimi/simsimi');
+// Định danh cho *client.player* để dẽ dàng dùng nó.
+client.player = player;
+
+// Login to Discord
+client.login(token);
+
+// Ready
+client.once('ready', function () {
+    console.log('Logged as in [' + client.user.tag + ']');
+    client.user.setActivity("music ♪", { 
+        type: "PLAYING" 
+    });
+});
+
+
 
 // Start message
-onMessage(async message => {
+client.on('messageCreate', async message => {
     const content = message.content;
-    if (content.split(' ')[0] === prefix + commands.sim) {
-        const msg = content.slice(prefix.length + commands.sim.length).trim();
-        simCommand(msg, message);
+    let guildQueue = client.player.getQueue(message.guild.id);
+    // Lệnh tìm nhạc và phát nhạc
+    if (content.split(' ')[0] === prefix + 'play') {
+        const query = message.content.slice(prefix.length + 'play'.length).trim();
+        let queue = client.player.createQueue(message.guild.id);
+        await queue.join(message.member.voice.channel);
+        let song = await queue.play(query).catch(_ => {
+            if(!guildQueue)
+                queue.stop();
+        });
     }
 
-    if (content.split(' ')[0] === prefix + commands.play) {
-        const data = message.content.slice(prefix.length + commands.play.length).trim();
-        playCommand(data, message);
+    // Lệnh chuyển nhạc
+    if (content.split(' ')[0] === prefix + 'skip') {
+        guildQueue.skip();
     }
 
-    if (content.split(' ')[0] === prefix + commands.next) {
-        nextCommand(message);
+    // Lệnh xoá tất cả nhạc
+    if (content.split(' ')[0] === prefix + 'clear') {
+        guildQueue.clearQueue();
     }
 
-    if (content.split(' ')[0] === prefix + commands.clear) {
-        clearCommand(message);
+    // Lệnh lặp lại bài hát 
+    // if (content.split(' ')[0] === prefix + 'loop') {
+    //     loopCommand(message);
+    // }
+
+    // Lệnh dừng nhạc
+    if (content.split(' ')[0] === prefix + 'pause') {
+        guildQueue.setPaused(true);
     }
 
-    if (content.split(' ')[0] === prefix + commands.loop) {
-        loopCommand(message);
-    }
-
-    if (content.split(' ')[0] === prefix + commands.list) {
-        listCommand(message);
-    }
-
-    if (content.split(' ')[0] === prefix + commands.pause) {
-        pauseCommand(message);
-    }
-
-    if (content.split(' ')[0] === prefix + commands.resume) {
-        resumeCommand(message);
+    // Lệnh tiếp tục nhạc
+    if (content.split(' ')[0] === prefix + 'resume') {
+        guildQueue.setPaused(false);
     }
 });
