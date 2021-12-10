2b7342b470fa878bc3d091d76fc264e7959c2e79
kayy0812 <kayy0812.levanloc@gmail.com>
2021-12-10 12:40:24 +0700

    updating ...

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ client.once('ready', function () {
     });
 });
 
-
-
 // Start message
 client.on('messageCreate', async message => {
     const content = message.content;
@@ -38,9 +36,27 @@ client.on('messageCreate', async message => {
     // Lệnh tìm nhạc và phát nhạc
     if (content.split(' ')[0] === prefix + 'play') {
         const query = message.content.slice(prefix.length + 'play'.length).trim();
-        let queue = client.player.createQueue(message.guild.id);
+        let queue = client.player.createQueue(message.guild.id, {
+            data: {
+                message: message
+            }
+        });
         await queue.join(message.member.voice.channel);
         let song = await queue.play(query).catch(_ => {
+            if(!guildQueue)
+                queue.stop();
+        });
+    }
+
+    if (content.split(' ')[0] === prefix + 'playlist') {
+        const query = message.content.slice(prefix.length + 'playlist'.length).trim();
+        let queue = client.player.createQueue(message.guild.id, {
+            data: {
+                message: message
+            }
+        });
+        await queue.join(message.member.voice.channel);
+        let song = await queue.playlist(query).catch(_ => {
             if(!guildQueue)
                 queue.stop();
         });
@@ -56,11 +72,6 @@ client.on('messageCreate', async message => {
         guildQueue.clearQueue();
     }
 
-    // Lệnh lặp lại bài hát 
-    // if (content.split(' ')[0] === prefix + 'loop') {
-    //     loopCommand(message);
-    // }
-
     // Lệnh dừng nhạc
     if (content.split(' ')[0] === prefix + 'pause') {
         guildQueue.setPaused(true);
@@ -70,4 +81,37 @@ client.on('messageCreate', async message => {
     if (content.split(' ')[0] === prefix + 'resume') {
         guildQueue.setPaused(false);
     }
+
+    client.player
+        // Emitted when channel was empty.
+        .on('channelEmpty',  (queue) =>
+            console.log(`Everyone left the Voice Channel, queue ended.`))
+        // Emitted when a song was added to the queue.
+        .on('songAdd',  (queue, song) =>
+            queue.data.message.channel.send(`${song.name}`))
+        // Emitted when a playlist was added to the queue.
+        .on('playlistAdd',  (queue, playlist) =>
+            console.log(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+        // Emitted when there was no more music to play.
+        .on('queueDestroyed',  (queue) =>
+            console.log(`The queue was destroyed.`))
+        // Emitted when the queue was destroyed (either by ending or stopping).    
+        .on('queueEnd',  (queue) =>
+            console.log(`The queue has ended.`))
+        // Emitted when a song changed.
+        .on('songChanged', (queue, newSong, oldSong) =>
+            console.log(`${newSong} is now playing.`))
+        // Emitted when a first song in the queue started playing.
+        .on('songFirst',  (queue, song) =>
+            queue.data.message.channel.send(`${song.name}`))
+        // Emitted when someone disconnected the bot from the channel.
+        .on('clientDisconnect', (queue) =>
+            console.log(`I was kicked from the Voice Channel, queue ended.`))
+        // Emitted when deafenOnJoin is true and the bot was undeafened
+        .on('clientUndeafen', (queue) =>
+            console.log(`I got undefeanded.`))
+        // Emitted when there was an error in runtime
+        .on('error', (error, queue) => {
+            console.log(`Error: ${error} in ${queue.guild.name}`);
+    });
 });
